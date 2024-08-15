8782e097a1d67ffbb67e889d176f10ff3e8fbbd6
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-15 18:50:52 -0400

    fix repositorio de ingresos

--- src/main/java/com/monedero/digital/repositories/IncomeRepository.java
+++ src/main/java/com/monedero/digital/repositories/IncomeRepository.java
@@ -2,6 +2,8 @@ package com.monedero.digital.repositories;
 
 import com.monedero.digital.entities.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
 }
