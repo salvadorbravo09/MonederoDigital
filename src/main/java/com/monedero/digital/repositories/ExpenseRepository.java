81f1221bd851b31839f67439c8a80de6d1326ba1
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-15 17:20:32 -0400

    se creo el repositorio para la entidad de gasto

---
+++ src/main/java/com/monedero/digital/repositories/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.monedero.digital.repositories;
+
+import com.monedero.digital.entities.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
