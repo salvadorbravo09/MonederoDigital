aab8c1dd210d94fd7400b484d4da5ed82338f56f
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-15 17:18:07 -0400

    primer commit

---
+++ src/main/java/com/monedero/digital/MonederoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.monedero.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonederoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonederoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monedero/digital/MonederoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monedero.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonederoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
