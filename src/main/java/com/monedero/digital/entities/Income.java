68d8502cb501b6d9af5718def66d69ff75995076
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-15 19:00:22 -0400

    se creo la entidad para los ingresos

--- src/main/java/com/monedero/digital/entities/Income.java
+++ src/main/java/com/monedero/digital/entities/Income.java
@@ -1,5 +1,6 @@
 package com.monedero.digital.entities;
 
+import com.monedero.digital.dtos.IncomeDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,4 +26,16 @@ public class Income {
     private LocalDate date;
 
     private Integer amount;
+
+    public IncomeDTO getIncomeDto() {
+        IncomeDTO incomeDTO = new IncomeDTO();
+        incomeDTO.setId(id);
+        incomeDTO.setTitle(title);
+        incomeDTO.setAmount(amount);
+        incomeDTO.setCategory(category);
+        incomeDTO.setDescription(description);
+        incomeDTO.setDate(date);
+
+        return incomeDTO;
+    }
 }
